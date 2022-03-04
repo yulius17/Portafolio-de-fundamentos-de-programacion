a99395e4e76321d08c77e04ed9fe87adb124b2fb
yulius17 <yulius1718@outlook.es>
2022-03-04 03:35:02 -0500

    Update suma1.py

--- suma/suma1.py
+++ suma/suma1.py
@@ -1,10 +1,12 @@
 #Ejercicio de suma 1
 
+#creamos una variable a la cual le damos los valores a sumar y procedemos a imprimirla
+
 n = 1 + 5 + 4 + 10
 
 print(n)
 
+#[output] 20
 
-n2 = 20
 
-print(n + n2)
+
