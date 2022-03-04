d9887aaa9b7893d652ff814c4134ecb09ce99ea9
yulius17 <yulius1718@outlook.es>
2022-03-04 14:07:03 -0500

    Create str.py

---
+++ datos en python/str.py
@@ -0,0 +1,8 @@
+#Ejercicio de String 
+#Usamos comillas para escribir caracteres
+
+nombre = str(input("ingrese su nombre: "))
+
+col_fav = str(input("diga su color favorito: "))
+
+print("su nombre es:", nombre, "y su color favorito es: ", col_fav)
