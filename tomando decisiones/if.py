9354c8c068fe3270cb95bf20ee3874124fd691f4
yulius17 <yulius1718@outlook.es>
2022-03-04 14:34:46 -0500

    Update if.py

---
+++ tomando decisiones/if.py
@@ -0,0 +1,14 @@
+#ejercicio de sentencia if
+
+#pedir al usuario un valor, si el valor es positivo
+#pedir un segundo valor y calcular la suma la resta y producto de ambos
+
+num1 = int(input("ingrese un numero:" ))
+
+if num1 > 0: 
+    num2 = int(input("ingrese un numero:"))
+    print("suma:", num1 + num2)
+    print("resta:", num1 - num2)
+    print("producto:", num1 * num2)
+
+print("...")
