8275c8c95983ef0ebafa5c0330077462dbe856e4
yulius17 <yulius1718@outlook.es>
2022-03-04 03:29:15 -0500

    Create suma3.py

---
+++ suma/suma3.py
@@ -0,0 +1,15 @@
+#Ejercicio 3
+
+#En este ejercicio tomaremos 4 variables, las uniremos una sola llamada "suma" y lo imprimiremos
+
+a = 5
+
+b = 10 
+
+c = 15
+
+d = 20
+
+suma = (a + b + c + d)
+
+print(suma)
